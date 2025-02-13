2c89c0bce55b26324cfd9cf7fafbb259749bb00a
BarkJiHwan <wlghksdl159@naver.com>
2025-02-13 18:43:36 +0900

    CameraMove
    플레이어를 따라다니는 카메라 구현
    플레이어 move 마우스로 변경중

--- Assets/Scripts/TestScripts.cs
+++ Assets/Scripts/TestScripts.cs
@@ -2,27 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine.Utility;
 
 public class TestScripts : MonoBehaviour
 {
-    Transform tr;
-    Vector3 moveDir;
+    Vector3 Pos;
+    Vector3 dir;
+    Quaternion lookTarget;
 
-    void OnMove(InputValue val)
-    {
-        Vector2 dir = val.Get<Vector2>();
-        moveDir = new Vector3(dir.x,0,dir.y);
-    }
-    void OnAttack()
-    {
-        Debug.Log("공격");
-    }
+    public float moveSpeed = 5f;
+    bool isMoveable = false;
     void Update()
     {
-        if (moveDir != Vector3.zero)
+        if(Input.GetMouseButton(1))
         {
-            transform.rotation = Quaternion.LookRotation(moveDir);
-            transform.Translate(Vector3.forward * Time.deltaTime * 4.0f);
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.yellow, 1.0f); 
+                Pos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                Debug.Log(Pos);
+                dir = Pos - transform.position;
+                Debug.Log(dir);
+                lookTarget = Quaternion.LookRotation(dir);
+                isMoveable = true;
+            }
+        }
+        Move();
+        
+    }
+
+    void Move()
+    {
+        if(isMoveable == true)
+        {
+            transform.position += dir.normalized * Time.deltaTime * moveSpeed;
+            transform.rotation = Quaternion.Lerp(transform.rotation, lookTarget, 0.25f);
+            float dt = Vector3.Distance(transform.position, Pos);
+            if (dt <= 1f)
+            {
+                 isMoveable = false;
+            }
         }
     }
+
+    //Transform tr;
+    //Vector3 moveDir;
+
+    //void OnMove(InputValue val)
+    //{
+    //    Vector2 dir = val.Get<Vector2>();
+    //    moveDir = new Vector3(dir.x,0, dir.y);
+    //}
+    //void OnAttack()
+    //{
+    //    Debug.Log("공격");
+    //}
+    //void Update()
+    //{
+    //    if (moveDir != Vector3.zero)
+    //    {
+    //        transform.rotation = Quaternion.LookRotation(moveDir);
+    //        transform.Translate(Vector3.forward * Time.deltaTime * 4.0f);
+    //    }
+    //}
 }
